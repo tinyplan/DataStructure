82e1e078965596144928e475432f7f21889d0fd5
xzy <3405443386@qq.com>
2019-09-22 14:48:58 +0800

    单链表测试

---
+++ exam1/src/com/medium/first/model/Node.java
@@ -0,0 +1,24 @@
+package com.medium.first.model;
+
+/**
+ * 单链表结点类
+ */
+public class Node<T> {
+
+    public T data;//数据域,包含系数、指数的数组
+    public Node<T> next;//地址域,应用后继结点
+
+    public Node(){
+        this(null,null);
+    }
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    @Override
+    public String toString(){
+        return this.data.toString();
+    }
+}
