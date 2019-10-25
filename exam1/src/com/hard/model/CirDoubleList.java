eeb78e41b0bab29f2d24fd56add409c48af74022
xzy <3405443386@qq.com>
2019-10-25 20:03:18 +0800

    循环双链表实现

--- exam1/src/com/hard/model/CirDoubleList.java
+++ exam1/src/com/hard/model/CirDoubleList.java
@@ -1,15 +1,12 @@
 package com.hard.model;
 
-import com.medium.first.model.Node;
-import com.sun.xml.internal.txw2.output.IndentingXMLFilter;
-
 /**
  * 循环双链表类
  * @param <T>
  */
 public class CirDoubleList<T> {
     public DoubleNode<T> head;
-    private int size;
+    public int size;
 
     //三种构造方法
     public CirDoubleList() {
@@ -34,24 +31,11 @@ public class CirDoubleList<T> {
     public CirDoubleList(CirDoubleList<T> list){
         this();
         this.size = list.size;
-        DoubleNode[] nodes = new DoubleNode[size];
-        DoubleNode<T> p = list.head.next;
-        int i = 0;
-        while (p != list.head){
-            nodes[i] = new DoubleNode<>(p.data);
+        DoubleNode<T> p = list.head.next;//指向插入链表头结点
+        while(p != list.head){
+            this.insert(p.data);
             p = p.next;
-            i++;
         }
-        //赋予当前list
-        DoubleNode<T> target = this.head;
-        for(i = 0; i < size; i++){
-            target.next = nodes[i];
-            nodes[i].prev = target;
-            target = target.next;
-        }
-        //连接首尾结点
-        this.head.prev = nodes[size-1];
-        nodes[size-1].next = this.head;
     }
 
     public boolean isEmpty(){
@@ -75,10 +59,23 @@ public class CirDoubleList<T> {
                 break;
         }
         DoubleNode<T> target = this.head;//初始front指向头结点
-        //语句说明:当target.next指向头结点时,说明到达尾结点
-        //语句作用:使target指向index位置的前驱元素
+        /*  语句说明:当target.next指向头结点时,说明到达尾结点
+            语句作用:使target指向index位置的前驱元素*/
         for(int i = 0; target.next != this.head && i <= index; i++) target = target.next;
         return target;
+    }
+
+    @Deprecated
+    public DoubleNode<T>[] getNodes(CirDoubleList<T> list){
+        DoubleNode[] nodes = new DoubleNode[list.size];
+        DoubleNode<T> p = list.head.next;
+        int i = 0;
+        while (p != list.head){
+            nodes[i] = new DoubleNode<>(p.data);
+            p = p.next;
+            i++;
+        }
+        return nodes;
     }
 
     public T get(int index){
@@ -87,7 +84,8 @@ public class CirDoubleList<T> {
     }
 
     public void set(int index,T data){
-
+        DoubleNode<T> cur = this.getNode(index,0);
+        cur.data = data;
     }
 
     /**
@@ -127,6 +125,34 @@ public class CirDoubleList<T> {
         return insertNode;
     }
 
+    /**
+     * 插入不同的元素
+     * @param data
+     * @return
+     */
+    public DoubleNode<T> insertDifferent(T data){
+        if(this.search(data) != null) return null;
+        return insert(data);
+    }
+
+    /**
+     * 合并两张链表
+     * @param list
+     */
+    public void addAll(CirDoubleList<T> list){
+        this.size+=list.size;
+        DoubleNode<T> p = list.head.next;//指向插入链表头结点
+        while(p != list.head){
+            this.insert(p.data);
+            p = p.next;
+        }
+    }
+
+    /**
+     * 删除元素
+     * @param index 删除位置索引
+     * @return
+     */
     public T remove(int index){
         if(isEmpty() || index >= size) return null;
         DoubleNode<T> cur = this.getNode(index,0);
@@ -136,12 +162,35 @@ public class CirDoubleList<T> {
         return cur.data;
     }
 
+    /**
+     * 删除元素
+     * @param key 删除位置的元素
+     * @return
+     */
+    public T remove(T key){
+        DoubleNode<T> front = this.search(key);
+        if(front != null){
+            front.prev.next = front.next;
+            front.next.prev = front.prev;
+            size--;
+            return key;
+        }
+        return null;
+    }
+
     public T removeLast(){
         DoubleNode<T> rear = this.head.prev;
         this.head.prev = rear.prev;
         rear.prev.next = this.head;
         size--;
         return rear.data;
+    }
+
+    public DoubleNode<T> search(T key){
+        for(DoubleNode<T> p = this.head.next;p != this.head;p = p.next){
+            if(key.equals(p.data)) return p;
+        }
+        return null;
     }
 
     public void clear(){
@@ -157,12 +206,12 @@ public class CirDoubleList<T> {
     @Override
     public String toString(){
         DoubleNode<T> p = this.head.next;
-        StringBuilder str = new StringBuilder(this.getClass().getName()).append("(");
+        StringBuilder str = new StringBuilder().append("[\n");
         while(p != this.head){
             str.append(p.data.toString());
-            if(p != this.head.prev) str.append(",");
+            if(p != this.head.prev) str.append(",\n");
             p = p.next;
         }
-        return str.append(")").toString();
+        return str.append("\n]").toString();
     }
 }
